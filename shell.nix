b1a0405db00100e1275f2a4f4a083d25c4a3dca9
blezz-tech <markus.jenya04@yandex.ru>
2024-04-23 11:31:52 +0300

    update

--- shell.nix
+++ shell.nix
@@ -4,4 +4,14 @@ pkgs.mkShell {
   packages = [
     pkgs.jdk
   ];
+
+  shellHook = ''
+    mkdir -p build
+
+    javac -d build @<(find -name "*.java")
+
+    cd build
+
+    jar cvfm $PROGRAM_NAME /build/src/META-INF/MANIFEST.MF *
+  '';
 }
